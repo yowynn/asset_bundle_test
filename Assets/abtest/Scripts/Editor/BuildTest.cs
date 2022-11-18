af821aa6438cccd1ff8d6a5e9c6d04c0f57ac329
wuyuhong <wuyuhong@ihuman.com>
2022-11-18 19:54:22 +0800

    init

---
+++ Assets/abtest/Scripts/Editor/BuildTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class BuildAssetBundlesBuildMapExample : MonoBehaviour
+{
+    [MenuItem("Test/Build Asset Bundles Using BuildMap")]
+    static void BuildMapABs()
+    {
+        var assetBundleDirectory = "Assets/AssetBundles";
+        var abnames = new string[]
+        {
+            "mat.v1",
+            //"mat.v2",
+            "prefab",
+        };
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        AssetBundleBuild[] buildMap = new AssetBundleBuild[abnames.Length];
+        for (var i = 0; i < abnames.Length; i++)
+        {
+            string variant = null;
+            var abname = abnames[i];
+            var assetPaths = AssetDatabase.GetAssetPathsFromAssetBundle(abname);
+            var ss = abname.Split('.');
+            if (ss.Length == 2)
+            {
+                abname = ss[0];
+                //variant = ss[1];
+            }
+            AssetBundleBuild build = new AssetBundleBuild();
+            build.assetBundleName = abname;
+            build.assetBundleVariant = variant;
+            Debug.Log($"name: {abname}, variant: {variant}");
+            build.assetNames = assetPaths;
+            buildMap[i] = build;
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, buildMap, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
---
+++ Assets/abtest/Scripts/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var abPrefab = AssetBundle.LoadFromFile("AssetBundles/StandaloneWindows/prefab");
+        //var abPrefab = AssetBundle.LoadFromFile("Assets/AssetBundles/prefab");
+        var abMat = AssetBundle.LoadFromFile("AssetBundles/StandaloneWindows/mat");
+        //var abMatV1 = AssetBundle.LoadFromFile("AssetBundles/StandaloneWindows/mat.v1");
+        //var abMatV2 = AssetBundle.LoadFromFile("AssetBundles/StandaloneWindows/mat.v2");
+        //var abMatp = AssetBundle.LoadFromFile("Assets/AssetBundles/mat");
+        //var abMatV1p = AssetBundle.LoadFromFile("Assets/AssetBundles/mat.v1");
+        //var abMatV2p = AssetBundle.LoadFromFile("Assets/AssetBundles/mat.v2");
+        var prefab = abPrefab.LoadAsset<GameObject>("prefab1");
+        GameObject.Instantiate(prefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
